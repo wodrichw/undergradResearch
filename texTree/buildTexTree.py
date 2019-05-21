920764fe7262af05793d6b8f020fcfa5609fb3ce
William Wodrich <wodrichw@oregonstate.edu>
2019-05-20 23:57:04 -0700

    cleaning up master

--- texTree/buildTexTree.py
+++ texTree/buildTexTree.py
@@ -73,5 +73,5 @@ def buildTexTreeString(n):
     
     
 
-n = buildWordListTree(WordListNode(words=['0', '1']), 0, 11)
+n = buildWordListTree(WordListNode(words=['0', '1']), 0, 14)
 print(buildTexTreeString(n))
